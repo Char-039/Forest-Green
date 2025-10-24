a9a9984ef47a15f9d2100c4f903a751f499b799f
Jacob <pokemine007craft@gmail.com>
2025-10-23 22:29:33 -0700

    Added main menu functionality
    Made a script that switches scenes when a button is clicked. For now, with no scenes, nothing happens.

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Allows player to switch between scenes
+
+public class MainMenu : MonoBehaviour
+{
+    public void Continue() {
+        // SceneManager.LoadScene("Example");
+    }
+
+    public void NewGame() {
+        // SceneManager.LoadScene("Example");
+    }
+
+    public void LevelSelect() {
+        // SceneManager.LoadScene("Example");
+    }
+
+    public void Options() {
+        // SceneManager.LoadScene("Example");
+    }
+
+
+    public void QuitGame() {
+        // SceneManager.LoadScene("Example");
+    }
+}
