5dbadb66415f449e38ca23573beb9e3e50d20654
Jacob <pokemine007craft@gmail.com>
2025-10-27 15:23:04 -0700

    Slightly better mirror logic
    May be better to just duplicate the environment and have the reflection camera only track the player. That way the environment stays still and the player moves. Actually...I think I will implement that once we have an apartment set up.

--- Assets/Scripts/Objects/MirrorLogic.cs
+++ Assets/Scripts/Objects/MirrorLogic.cs
@@ -1,22 +1,30 @@
 using UnityEngine;
 
 public class MirrorLogic : MonoBehaviour {
+    public Camera mainCamera;
 
-    public Transform player;
-    public Transform mirror;
+    public Transform mirrorSurface;
+    public Transform reflectionCameraTransform;
 
-    void Start() {
-        
-    }
 
-    // Update is called once per frame
-    void Update() {
-        // Position of the player relative to the mirror
-        Vector3 localPlayer = mirror.InverseTransformPoint(player.position);
-        // Inverts position of player (bc it's a mirror)
-        transform.position = mirror.TransformPoint(new Vector3(localPlayer.x, localPlayer.y,  -localPlayer.z));
+    void LateUpdate() {
 
-        Vector3 lookAtMirror = mirror.TransformPoint(new Vector3(-localPlayer.x, localPlayer.y, localPlayer.z));
-        transform.LookAt(lookAtMirror);
+        if (mainCamera == null || mirrorSurface == null) return;
+
+        // Player position relative to mirror
+        Vector3 localPlayerPos = mirrorSurface.InverseTransformPoint(mainCamera.transform.position);
+
+        // Invert it so it's a reflection
+        Vector3 reflectedLocalPos = new Vector3(-localPlayerPos.x, localPlayerPos.y, localPlayerPos.z);
+        reflectionCameraTransform.position = mirrorSurface.TransformPoint(reflectedLocalPos);
+
+        // Find where player is facing
+        Vector3 cameraDirection = mainCamera.transform.forward;
+        // Find where mirror is facing
+        Vector3 mirrorNormal = mirrorSurface.forward;
+
+        // Reflect the camera's direction across the mirror
+        Vector3 reflectedDirection = Vector3.Reflect(cameraDirection, mirrorNormal);
+        reflectionCameraTransform.rotation = Quaternion.LookRotation(reflectedDirection);
     }
 }
